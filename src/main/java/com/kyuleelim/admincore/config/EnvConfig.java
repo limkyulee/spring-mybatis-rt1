77595a5a4af1f4016fa79ff2da562def23902b86
limkyulee <mud0308@gmail.com>
2025-04-21 10:24:12 +0900

    add env

--- src/main/java/com/kyuleelim/admincore/config/EnvConfig.java
+++ src/main/java/com/kyuleelim/admincore/config/EnvConfig.java
@@ -8,10 +8,12 @@ public class EnvConfig {
     static {
         // .env 파일을 읽음
         Dotenv dotenv = Dotenv.configure()
-                .ignoreIfMissing()
-                .load();
+            .directory("./")
+            .ignoreIfMissing()
+            .load();
 
         // 환경 변수 -> 시스템 프로퍼티로 설정
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
         System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
         System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
     }
