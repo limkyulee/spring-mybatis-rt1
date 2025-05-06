bf64e1a3a603376b09bb6e87a4f9942632d796d2
limkyulee <mud0308@gmail.com>
2025-05-06 20:33:22 +0900

    fix sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,9 @@
 -- USER
-INSERT INTO USER (username, password, email, use_yn) VALUES
+INSERT INTO USER (
+      USER_NM
+     ,PASSWORD
+     ,EMAIL
+     ,USE_YN
+) VALUES
     ('admin', 'admin1234', 'admin@example.com', 'Y'),
     ('testuser', 'test1234', 'user@example.com', 'Y');
